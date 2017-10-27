918b8fb94bb49b018826a54b649319a730b631f9
Thiago Dutra <thiagodutra@gmail.com>
2017-10-27 10:10:57 -0300

    Adicionado métodos recursivos a lista encadeada

--- linkedlists/ds/lists/SinglyLinkedList.java
+++ linkedlists/ds/lists/SinglyLinkedList.java
@@ -2,15 +2,19 @@ package ds.lists;
 
 public class SinglyLinkedList {
 	private Node first;
+	private Node nil;
+	
 
 	public SinglyLinkedList() {
-
+		nil.setNext(null);
+		first.setNext(nil);
 	}
 
 	public boolean isEmpty() {
 		return first == null;
 	}
-
+	
+	//Métodos Iterativos.
 	// Usado para inserir no inicio da lista
 	public void insertFirst(int element) {
 		Node newNode = new Node();
@@ -27,14 +31,16 @@ public class SinglyLinkedList {
 
 	public void insertLast(int element) {
 		Node atual = first;
-		while (atual.getNext() != null) {
+		while (atual.getNext() != nil) {
 			atual = atual.getNext();
 		}
 		Node newNode = new Node();
-		// newNode.setNext(null);
+		newNode.setNext(nil);
 		newNode.setElement(element);
 		atual.setNext(newNode);
 	}
+	
+	
 
 	public void printList() {
 		System.out.println("Lista (Primeiro --> Fim) ");
@@ -44,5 +50,36 @@ public class SinglyLinkedList {
 			atual = atual.getNext();
 		}
 	}
+	
+	//Inicio dos Métodos Recursivos
+	
+	public void printListRecursively (Node atual)
+	  {
+		atual = first;
+	    if (atual == null)
+	      return;
+	    else {
+	      System.out.println(atual.getElement());
+	      printListRecursively(atual.getNext());
+	    }
+	  }
+	
+	public int lengthRecursively (Node l)
+	  {
+	    if (l == null)
+	      return 0;
+	    else
+	      return 1 + lengthRecursively(l.getNext());
+	  }
 
+	public Node findRecursively (Node l, int value)
+	  {
+	    if (l == null)
+	      return null;
+	    else
+	      if (l.getElement() == value)
+	        return l;
+	      else
+	        return findRecursively(l.getNext(), value);
+	  }
 }
