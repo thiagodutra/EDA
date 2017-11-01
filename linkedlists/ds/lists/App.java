e4d15125873f4c98b5fc7cbab303975c50945241
Thiago Dutra <thiagodutra@gmail.com>
2017-11-01 11:58:33 -0300

    Fixed some recursvive methods for the SinglyLinkedList

--- linkedlists/ds/lists/App.java
+++ linkedlists/ds/lists/App.java
@@ -11,7 +11,24 @@ public class App {
 		a.insertFirst(21);	a.printList();
 		a.insertFirst(531);	a.printList();
 		a.insertLast(15);	a.printList();
-
+		System.out.println(a.findRecursively(a.getFirst(), 531));
+		System.out.println(a.findRecursively(a.getFirst(), 18));
+		System.out.println(a.lengthRecursively(a.getFirst()));
+		System.out.println(a.isEmpty());
+		a.deleteFirst(); 	a.printList();
+		a.deleteFirst();	a.printList();
+		a.deleteFirst();	a.printList();
+		a.deleteFirst();	a.printList();
+		a.deleteFirst();	a.printList();
+		a.deleteFirst();	a.printList();
+		System.out.println(a.lengthRecursively(a.getFirst()));
+		System.out.println(a.isEmpty());
+		a.insertFirst(10);  a.printList();
+		a.insertFirst(11);	a.printList();
+		a.insertLast(13);	a.printListRecursively(a.getFirst());
+		System.out.println(a.findRecursively(a.getFirst(), 13));
+		System.out.println(a.lengthRecursively(a.getFirst()));
+		System.out.println(a.isEmpty());
 	}
 
 }
--- linkedlists/ds/lists/SinglyLinkedList.java
+++ linkedlists/ds/lists/SinglyLinkedList.java
@@ -2,19 +2,16 @@ package ds.lists;
 
 public class SinglyLinkedList {
 	private Node first;
-	private Node nil;
-	
 
 	public SinglyLinkedList() {
-		nil.setNext(null);
-		first.setNext(nil);
 	}
 
 	public boolean isEmpty() {
 		return first == null;
 	}
 	
-	//Métodos Iterativos.
+	// ----------------> Inicio dos Métodos Iterativos <----------------
+	
 	// Usado para inserir no inicio da lista
 	public void insertFirst(int element) {
 		Node newNode = new Node();
@@ -22,26 +19,24 @@ public class SinglyLinkedList {
 		newNode.setNext(first);
 		first = newNode;
 	}
-
+	//Deleta o primeiro elemento
 	public Node deleteFirst() {
 		Node temp = first;
 		first = first.getNext();
 		return temp;
 	}
-
+	//Insere no fim
 	public void insertLast(int element) {
 		Node atual = first;
-		while (atual.getNext() != nil) {
+		while (atual.getNext() != null) {
 			atual = atual.getNext();
 		}
 		Node newNode = new Node();
-		newNode.setNext(nil);
+		newNode.setNext(null);
 		newNode.setElement(element);
 		atual.setNext(newNode);
 	}
-	
-	
-
+	//Imprime os elementos da lista
 	public void printList() {
 		System.out.println("Lista (Primeiro --> Fim) ");
 		Node atual = first;
@@ -51,35 +46,38 @@ public class SinglyLinkedList {
 		}
 	}
 	
-	//Inicio dos Métodos Recursivos
+	// ----------------> Inicio dos Métodos Recursivos <----------------
 	
-	public void printListRecursively (Node atual)
-	  {
-		atual = first;
-	    if (atual == null)
+	//Imprime Recursivamente
+	public void printListRecursively (Node list) {
+		if (list == null)
 	      return;
 	    else {
-	      System.out.println(atual.getElement());
-	      printListRecursively(atual.getNext());
+	      System.out.println(list.getElement());
+	      printListRecursively(list.getNext());
 	    }
 	  }
-	
-	public int lengthRecursively (Node l)
-	  {
-	    if (l == null)
+	//Retorna o tamanho da lista
+	public int lengthRecursively (Node list) {
+	    if (list == null)
 	      return 0;
 	    else
-	      return 1 + lengthRecursively(l.getNext());
+	      return 1 + lengthRecursively(list.getNext());
 	  }
-
-	public Node findRecursively (Node l, int value)
-	  {
-	    if (l == null)
-	      return null;
+	//Procura um elemento na lista
+	public int findRecursively (Node list, int value) {
+	    if (list == null)
+	      return -1;
 	    else
-	      if (l.getElement() == value)
-	        return l;
+	      if (list.getElement() == value)
+	        return list.getElement();
 	      else
-	        return findRecursively(l.getNext(), value);
+	        return findRecursively(list.getNext(), value);
 	  }
+	
+	//Getters Setters
+	public Node getFirst() {
+		return first;
+	}
+
 }
